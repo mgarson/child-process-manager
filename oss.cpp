253b4cb72d99631a8e86e849148e1f6cf023cda6
maija garson <maijagarson@yahoo.com>
2025-02-09 11:37:17 -0600

    Added user input validation when parsing options

--- oss.cpp
+++ oss.cpp
@@ -18,13 +18,20 @@ void print_usage(const char * app)
 
 int main(int argc, char *argv[])
 {
+	//Set default values
+	
+	int proc = 1;
+	int simul = 1;
+	int iter = 1;
+
+
+	// Value to keep track of child iterations
+	int count = 0;
+	
 	const char optstr[] = "hn:s:t:"; // options h, n, s, t
 	char opt;
-	string str;
-	int proc = 0;
-	int simul = 0;
-	int iter = 0;
-	int count = 0;
+	
+	
 
 
 	while ( ( opt = getopt (argc, argv, optstr ) ) != -1 )
@@ -35,35 +42,75 @@ int main(int argc, char *argv[])
 			print_usage( argv[0] );
 			return ( EXIT_SUCCESS );
 		case 'n':
-			if (optarg == NULL)
+			if (optarg[0] == '-')
 			{
-				fprintf(stderr, "Error! Option n needs to have a number");
-				return EXIT_FAILURE;
+				if(optarg[1] == 's' || optarg[1] == 't' || optarg[1] == 'h')
+				{
+					fprintf(stderr, "Error! Option n requires an argument.\n");
+					print_usage(argv[0]);
+					return EXIT_FAILURE;
+				}
+				else
+				{
+					fprintf(stderr, "Error! Invalid input.\n");
+					print_usage(argv[0]);
+					return EXIT_FAILURE;
+				}
 			}
 			for(int i = 0; optarg[i] != '\0'; i++)
 			{
 				if (!isdigit(optarg[i]))
 				{
 					fprintf(stderr, "Error! %s is not a valid number.\n", optarg);
+					print_usage(argv[0]);
 					return EXIT_FAILURE;
 				}
 			}
 			proc = atoi(optarg);
-			cout << proc << " set as n"<<endl;
 			break;
 		case 's':
+			if (optarg[0] == '-')
+			{
+				if (optarg[1] == 'n' || optarg[1] == 't' || optarg[1] == 'h')
+				{
+					fprintf(stderr, "Error! Option s requires an argument.\n");
+					print_usage(argv[0]);
+					return EXIT_FAILURE;
+				}
+				else
+				{
+					fprintf(stderr, "Error! Invalid input.\n");
+					print_usage(argv[0]);
+					return EXIT_FAILURE;
+				}
+			}
 			for(int i = 0; optarg[i] != '\0'; i++)
 			{
 				if (!isdigit(optarg[i]))
 				{
 					fprintf(stderr, "Error! %s is not a valid number.\n", optarg);
+					print_usage(argv[0]);
 					return EXIT_FAILURE;
 				}
 			}
 			simul = atoi(optarg);
-			cout << simul << " set as s" << endl;
 			break;
 		case 't':
+			if (optarg[0] == '-')
+			{
+				if (optarg[1] == 'n' || optarg[1] == 's' || optarg[1] == 'h')
+				{
+					fprintf(stderr, "Error! Option t requires an argument.\n");
+					print_usage(argv[0]);
+					return EXIT_FAILURE;
+				}
+				else
+				{
+					fprintf(stderr, "Error! Invalid input.\n");
+					print_usage(argv[0]);
+					return EXIT_FAILURE;
+				}
+			}
 			for(int i = 0; optarg[i] != '\0'; i++)
 			{
 				if (!isdigit(optarg[i]))
@@ -73,7 +120,6 @@ int main(int argc, char *argv[])
 				}
 			}
 			iter = atoi(optarg);
-			cout << iter << " set as t" << endl;
 			break;
 		default:
 			printf("Invalid option %c\n", optopt);
@@ -81,8 +127,8 @@ int main(int argc, char *argv[])
 			return EXIT_FAILURE;
 		}
 	}
-
-	str = to_string(iter);
+	fprintf(stdout, "Amount of processes: %d\nAmount that are simultaneous: %d\nAmount of iterations: %d.\n", proc, simul, iter);
+	string str = to_string(iter);
 	char* arg = new char[str.length()+1];
 	strcpy(arg, str.c_str());
 	while (count < proc)
